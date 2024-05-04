7859f0a69da98e6e3127f2c051572f04489e3445
orkag <orkag81@gmail.com>
2024-05-04 23:56:23 +0200

    first version

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//wprowadzenie statkow
+//grasz ze znajomym czy komputerem
+//jakiej strategi ma uzyc komputer 
+//kolorki
+//rozne ai
+//statki z pliku
+
+
+#include <bits/stdc++.h>
+
+int main()
+{
+  std::cout << "Welcome in battleships! " << std::endl;
+
+  std::string answer;
+
+  while(answer!="c"&&answer!="f")
+  {
+  std::cout << "Are you going to play with computer or with friend? c/f" << std::endl;
+  std::cin >> answer;
+  }
+
+  int x,y;
+
+  std::cout << "Enter grid x:  " << std::endl;
+  std::cin >> x;
+
+  std::cout << "Enter grid y:  " << std::endl;
+  std::cin >> y;
+
+
+
+
+
+}
